a464014450592c6ff29e36368cd68e01181bddeb
gyheo <heo.geoyeo@gmail.com>
2019-12-23 23:49:59 +0900

    Creating first component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Sojourner from './sojoruner';
 
 function App() {
   return (
-    <div><h1>Hello, React & Virtual DOM!</h1></div>
+    <div>
+      <h1>Hello, React & Virtual DOM!</h1>
+      <Sojourner/>
+    </div>
   );
 }
 
---
+++ src/sojoruner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sojourner(){
+    return <h3>I love traveling</h3>
+}
+
+export default Sojourner;
