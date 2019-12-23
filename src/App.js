b2ceb7a0981ab74e35cf6e9fcbe185aa3260f2a1
gyheo <heo.geoyeo@gmail.com>
2019-12-24 01:40:43 +0900

    Practice resuable components and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Traveler from './traveler';
 
-function Sojourner() {
-  return <h3>I am a sojourner</h3>;
+function Jobs({ calling }) {
+  return <h1>I am a {calling}</h1>;
 }
 
 function Food({ favorite }) {
   return <h1>I like a {favorite}</h1>;
 }
+
 function App() {
   return (
     <div>
       <h1>Hello, React & Virtual DOM!</h1>
-      <Sojourner/>
       <Traveler/>
+      <Jobs calling="sojourner"/>
+      <Jobs calling="traveler"/>
+      <Jobs calling="writer"/>
+      <Jobs calling="programmer"/>
       <Food favorite="kimchi"/>
       <Food favorite="ramen"/>
       <Food favorite="samgiopsal"/>
