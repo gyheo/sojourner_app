39ec9f7ddcbacdd43dad14d2b1d3af0833b3a352
gyheo <heo.geoyeo@gmail.com>
2019-12-24 01:38:05 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import Sojourner from './sojoruner';
 import Traveler from './traveler';
 
+function Sojourner() {
+  return <h3>I am a sojourner</h3>;
+}
+
+function Food({ favorite }) {
+  return <h1>I like a {favorite}</h1>;
+}
 function App() {
   return (
     <div>
       <h1>Hello, React & Virtual DOM!</h1>
       <Sojourner/>
       <Traveler/>
+      <Food favorite="kimchi"/>
+      <Food favorite="ramen"/>
+      <Food favorite="samgiopsal"/>
+      <Food favorite="chukumi"/>
     </div>
   );
 }
--- src/sojoruner.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Sojourner(){
-    return <h3>I am a sojourner</h3>
-}
-
-export default Sojourner;
