fd3224600886f8c51a360684ad29ae5239b7ca4a
gyheo <heo.geoyeo@gmail.com>
2019-12-27 23:47:24 +0900

    .map Recap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Interest({ name, picture }) {
   return (
     <div>
       <h1>I'm interested in {name}</h1>
-      <img src={picture}/>
+      <img src={picture} alt={name}/>
     </div>
   );
 }
@@ -14,29 +14,34 @@ function Food({ name, picture }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <img src={picture} alt={name}/>
     </div>
   );
 }
 
 const calling = [
   {
+    id: 1,
     name: "sojourner",
     image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Sojourner_on_Mars_PIA01122.jpg"
   },
   {
+    id: 2,
     name: "labor",
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Modern_Times_poster.jpg/440px-Modern_Times_poster.jpg"
   },
   {
+    id: 3,
     name: "guitar",
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Guitar_1.jpg/600px-Guitar_1.jpg"
   },
   {
+    id: 4,
     name: "running",
     image: "https://cdn.pixabay.com/photo/2019/07/22/23/44/runner-4356298_960_720.jpg"
   },
   {
+    id: 5,
     name: "programming",
     image: "https://upload.wikimedia.org/wikipedia/commons/2/24/Wyvern-programming-languages-in-one.jpg"
   }
@@ -44,25 +49,24 @@ const calling = [
 
 const foodILike = [
   {
+    id: 1,
     name: "Bibimbap",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/800px-Dolsot-bibimbap.jpg"
   },
   {
+    id: 2,
     name: "Kimchi",
     image:
     "https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg"
   },
   {
+    id: 3,
     name: "Kimbap",
     image:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/440px-Vegetable_gimbap.jpg"
   }
 ];
-
-function renderFood(dish){
-  console.log(dish)
-}
 
 function App() {
   return (
@@ -70,9 +74,11 @@ function App() {
       <h1>Hello, React & Virtual DOM!</h1>
       <Traveler />
       {calling.map(values => (
-        <Interest name={values.name} picture={values.image}/>
+        <Interest key={values.id} name={values.name} picture={values.image}/>
       ))}
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
